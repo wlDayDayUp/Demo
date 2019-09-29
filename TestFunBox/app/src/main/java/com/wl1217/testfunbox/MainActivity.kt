5694056880be0017a259e847fde0d9d56a1ed26d
wl1217 <13773567079@163.com>
2019-09-29 15:51:01 +0800

    init TestFunBox Project

---
+++ TestFunBox/app/src/main/java/com/wl1217/testfunbox/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wl1217.testfunbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

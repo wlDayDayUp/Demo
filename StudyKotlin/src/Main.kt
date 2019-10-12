288f5571a4a055e4cb52a58f35a8c9b8bbc86d8a
wl1217 <13773567079@163.com>
2019-10-12 08:49:51 +0800

    新建kotlin 学习项目

---
+++ StudyKotlin/src/Main.kt
@@ -0,0 +1,10 @@
+import bean.UserBean
+
+fun main() {
+    println("Hello Kotlin")
+
+    val userBean = UserBean("wg")
+
+    println(userBean.name)
+
+}
---
+++ StudyKotlin/src/bean/UserBean.kt
@@ -0,0 +1,3 @@
+package bean
+
+data class UserBean(val name: String)
